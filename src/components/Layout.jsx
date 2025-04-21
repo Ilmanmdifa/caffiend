cc06e0f5aee9396937c4f428a0a02b811d2632a7
Ilmanmdifa <ilmanmdifa63@gmail.com>
2025-04-21 14:44:41 +0700

    add tittle, create layout for homepage and form

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -1,9 +1,35 @@
 export default function Layout(props) {
   const { children } = props;
 
-  const header = <header></header>;
+  const header = (
+    <header>
+      <div>
+        <h1 className="text-gradient">CAFFIEND</h1>
+        <p>For Coffee Insatiates</p>
+      </div>
+      <button>
+        <p>Sign up for free</p>
+        <i class="fa-solid fa-mug-hot"></i>
+      </button>
+    </header>
+  );
 
-  const footer = <footer></footer>;
+  const footer = (
+    <footer>
+      <p>
+        <span className="text-gradient">Caffiend</span> was made by {" "}
+        <a href="http://www.github.com/ilmanmdifa" target="_blank">
+          ilmanmdifa
+        </a>{" "}
+        inspired by{" "}
+        <a href="http://www.smoljames.com" target="_blank">
+          smoljames
+        </a>{" "}
+        using the <a href="http://fantacss.smoljames.com">FantaCSS</a> design
+        library.
+      </p>
+    </footer>
+  );
 
   return (
     <>
